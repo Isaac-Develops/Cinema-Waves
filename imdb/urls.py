410dd3859d25b4941b006b2162d5520ec8a0b658
Isaac-Develops <isaacben822@gmail.com>
2021-10-07 12:03:27 -0400

    Added user detail view and template

--- imdb/urls.py
+++ imdb/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from profiles.views import user_profile_view
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('users/<int:id>/', user_profile_view, name='profiles'),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+from .models import User
+from reviews.models import Review
+
 # Create your views here.
+def user_profile_view(request, id):
+    user = User.objects.get(id=id)
+    reviews = Review.objects.filter(author=user)
+    return render(request, 'user_detail.html', {'user': user, 'reviews': reviews})
