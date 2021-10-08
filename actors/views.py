ee7ceb29a90e657621052db3ddb2e51569407461
Isaac-Develops <isaacben822@gmail.com>
2021-10-08 10:04:35 -0400

    Added actor detail view

--- actors/views.py
+++ actors/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
+from .models import Actor
+
 # Create your views here.
+def actor_detail_view(request, id):
+    actor = Actor.objects.get(id=id)
+    return render(request, 'actor_detail.html', {'actor': actor})
--- imdb/urls.py
+++ imdb/urls.py
@@ -19,10 +19,12 @@ from django.urls import path
 from profiles.views import user_profile_view
 from reviews.views import review_detail_view
 from movies.views import movie_detail_view
+from actors.views import actor_detail_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/<int:id>/', user_profile_view, name='profiles'),
     path('reviews/<int:id>/', review_detail_view, name='reviews'),
     path('movies/<int:id>/', movie_detail_view, name='movies'),
+    path('actors/<int:id>/', actor_detail_view, name='actors'),
 ]
