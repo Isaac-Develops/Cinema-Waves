4d0a31ec6c8f53b31b6f86cb48ca8787e2e80579
Gail Giles <73804018+gilesgr12@users.noreply.github.com>
2021-10-08 11:13:41 -0400

    adding to landing page

--- movies/admin.py
+++ movies/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
+from .models import BannerCards, Cards
+
 
 from .models import Movie
 
 # Register your models here.
 admin.site.register(Movie)
+admin.site.register(BannerCards)
+admin.site.register(Cards)
--- movies/views.py
+++ movies/views.py
@@ -1,7 +1,31 @@
-from django.shortcuts import render
-
+from django.shortcuts import redirect, render
+from .models import BannerCards, Cards
+from django.contrib.auth.models import auth
 # Create your views here.
 
 
 def home(request):
-    return render(request, "Home/home.html")
+    bannerCards = BannerCards.objects.all()
+    cards = Cards.objects.all()
+    context = {
+        'bannerCards': bannerCards,
+        'cards': cards,
+    }
+    return render(request, "Home/home.html", context)
+
+
+def search(request):
+    query = request.GET['query']
+    title = Cards.objects.filter(title__icontains=query)
+    category = Cards.objects.filter(category__icontains=query)
+    allCards = title.union(category)
+    context = {
+        'allCards': allCards,
+        "query": query,
+    }
+    return render(request, "Home/search.html", context)
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect("/")
