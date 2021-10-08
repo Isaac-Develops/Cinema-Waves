fb1203b41e541151d18a66e476c292228741ed95
Isaac-Develops <isaacben822@gmail.com>
2021-10-07 23:58:17 -0400

    Removed templates

--- imdb/settings.py
+++ imdb/settings.py
@@ -65,7 +65,7 @@ ROOT_URLCONF = 'imdb.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [ BASE_DIR / 'templates' ],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
