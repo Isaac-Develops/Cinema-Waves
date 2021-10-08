1e002fadf5ed59e142ab2a8400d4b967ee959e26
Isaac-Develops <isaacben822@gmail.com>
2021-10-08 09:54:39 -0400

    Added movie cast to movie detail view

--- movies/views.py
+++ movies/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render
 
 from .models import Movie
 from reviews.models import Review
+from actors.models import Actor
 
 # Create your views here.
 def movie_detail_view(request, id):
     movie = Movie.objects.get(id=id)
     reviews = Review.objects.filter(movie=movie)
-    return render(request, 'movie_detail.html', {'movie': movie, 'reviews': reviews})
+    cast = movie.actor_set.all()
+    return render(request, 'movie_detail.html', {'movie': movie, 'reviews': reviews, 'cast': cast})
