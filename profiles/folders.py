da6c540d881e375117d6de0a52ff4fa43fc3aa40
occie-holloway <universalmediaxchange@gmail.com>
2021-10-11 17:34:43 -0400

    new updated authentication

---
+++ profiles/folders.py
@@ -0,0 +1,11 @@
+from django import forms
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(max_length=150)
+    password = forms.CharField(widget=forms.PasswordInput)
+
+
+class SignUpForm(forms.Form):
+    username = forms.CharField(max_length=150)
+    password = forms.CharField(widget=forms.PasswordInput)
