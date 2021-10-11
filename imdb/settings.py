53557a004abcade1c5f2c880c2a3f683c8657e61
Isaac-Develops <isaacben822@gmail.com>
2021-10-11 18:10:10 -0400

    Added .env code to settings.py

--- imdb/settings.py
+++ imdb/settings.py
@@ -9,10 +9,17 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+
 from pathlib import Path
+import os
+import environ
+
+# Initialise environment variables
+env = environ.Env()
+
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -20,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n8-%g9*ud5&@og3*lj2)^2t#p^-vyu24kmck3=41wwdw#b6i2@'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
