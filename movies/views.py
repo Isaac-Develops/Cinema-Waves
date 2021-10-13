3524ec3ecdfce456fae2ccc9b5218e5545b138f4
Isaac-Develops <isaacben822@gmail.com>
2021-10-13 14:26:35 -0400

    Added 404 and 500 error views

--- movies/views.py
+++ movies/views.py
@@ -40,3 +40,11 @@ def search(request):
 def logout(request):
     auth.logout(request)
     return redirect("/")
+
+
+def page_not_found_view(request, exception):
+    return render(request, '404.html', status=404)
+
+
+def server_error_view(request):
+    return render(request, '500.html', status=500)
