d59183cbc2d6e4cac1289a1cd4f206819383a7bc
Isaac-Develops <isaacben822@gmail.com>
2021-10-13 17:40:49 -0400

    Added SignUpForm to forms.py

--- profiles/forms.py
+++ profiles/forms.py
@@ -4,3 +4,8 @@ from django import forms
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=50)
     password = forms.CharField(widget=forms.PasswordInput)
+
+
+class SignUpForm(forms.Form):
+    username = forms.CharField(max_length=150)
+    password = forms.CharField(widget=forms.PasswordInput)
