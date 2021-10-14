fc318380ee21079194671e686352fc212ef2db93
John <cooksta120021@gmail.com>
2021-10-14 09:19:01 -0500

    updated v2

--- movies/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-    path("", views.home, name="home"),
-    path("search/", views.search, name="search"),
-    path("logout/", views.logout, name="logout"),
-]
