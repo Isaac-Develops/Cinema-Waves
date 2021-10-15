7a5a0db36dae40ddf4c124743bab3ef031417430
occie-holloway <universalmediaxchange@gmail.com>
2021-10-15 13:50:39 -0400

    new updates in my branch

--- actors/models.py
+++ actors/models.py
@@ -7,7 +7,7 @@ class Actor(models.Model):
     name = models.CharField(max_length=100)
     gender = models.IntegerField()
     image = models.ImageField(
-        upload_to='actors', default='assets/default_movie.png')
+        upload_to='assets', default='default_movie.png')
     biography = models.TextField(null=True, blank=True)
     movies = models.ManyToManyField(Movie)
     birthday = models.DateField()
